c278202046f4c12490e825cd8ff49b5fbdaf0e2d
Zsailer <zachsailer@gmail.com>
2015-09-21 10:37:17 -0700

    small bug in phyml filename

--- phylogenetics/phyml.py
+++ phylogenetics/phyml.py
@@ -16,22 +16,22 @@ def run_phyml(homolog_set, outfile_prefix, dtype="aa", rm_tmp=False, *args, **kw
     # Create a temporary fasta file from homologs as input to CDHIT.
     fname = "%s.phy" % outfile_prefix
     homolog_set.write(fname, format="phylip")
-    
+
     # Build command array and run it.
     stuff = ("-i", fname, "-d", dtype)
     stuff += args
     run_subprocess("phyml", *stuff, **kwargs)
-    
+
     # Read the tree file and add to homologset.
-    f = open("%s.nwk" % outfile_prefix, "r")
+    f = open("%s.phy_phyml_tree.txt" % outfile_prefix, "r")
     tree = f.read()
     f.close()
-    
+
     homolog_set.tree = tree
     
     # Change extension of tree to nwk
-    shutil.move("%s.phy_phyml_tree.txt" % outfile_prefix, 
-                "%s.phy_phyml_tree.nwk" % outfile_prefix)
+    shutil.move("%s.phy_phyml_tree.txt" % outfile_prefix,
+                "%s.nwk" % outfile_prefix)
 
     # Remove alignment files if you want to just keep homologs.
     if rm_tmp:
