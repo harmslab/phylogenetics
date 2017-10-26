907cb7a58683968397d0ef38c49d03a1fdd18687
Zachary Sailer <zachsailer@gmail.com>
2017-10-26 09:15:51 -0700

    purge

--- setup.py
+++ setup.py
@@ -2,27 +2,14 @@
 from setuptools import setup
 
 setup(name = 'phylogenetics',
-    version = '0.3',
-    description = 'Python API that provides simple tools for doing phylogenetics.',
+    version = '1.0',
+    description = 'Human readable phylogenetics in Python.',
     author = 'Zach Sailer',
     author_email = 'zachsailer@gmail.com',
     url = 'https://github.com/Zsailer/phylogenetics',
-    download_url = 'https://github.com/Zsailer/phylogenetics/tarball/v0.3',
     packages = ['phylogenetics'],
     install_requires=[
         'dendropy',
-        'biopython'
-    ],
-    #scripts = 'scripts/blast-download',
-        #'scripts/blast-process',
-        #'scripts/blast-reverse',
-        #'scripts/blast-seeds',
-        #'scripts/phylo-add-align',
-        #'scripts/phylo-align',
-        #'scripts/phylo-cdhit',
-	    #'scripts/phylo-concat',
-        #'scripts/phylo-edit-names',
-        #'scripts/phylo-rm',
-        #'scripts/phylo-tree'],
+        'biopython'],
     zip_safe = False
 )
