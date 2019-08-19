114865bb48675ce44c3f430e68f4b11a2501cabc
Michael J. Harms <harmsm@gmail.com>
2019-08-19 10:20:11 -0700

    adding df read/write capabilities

--- phylogenetics/tools/df_editor.py
+++ phylogenetics/tools/df_editor.py
@@ -9,7 +9,7 @@ import phylopandas as phy
 import pandas as pd
 import numpy as np
 
-import random, string
+import random, string, os
 
 def _check_uid(df,uid):
     """
@@ -85,3 +85,172 @@ def split_sequence(df,uid,split_site):
     new_df = new_df.reset_index(drop=True)
 
     return new_df
+
+def to_external(out_file,df,id_col=None,out_type=None,overwrite=False):
+    """
+    Write a phylopandas data frame out to an external file.  If the user does
+    not edit the sequence ids in the extenral file, whatever edits the user
+    makes to the alignment or tree can be read back into the source data frame
+    using the from_external function.
+
+    out_file: output file.
+    df: phylopandas dataframe
+    id_col: id column to use as the id for each sequence
+    out_type: what type of file to write.  if None, try to figure out from the
+              file extension of the output file
+    overwrite: overwrite an existing output file.
+    """
+
+    out_methods = {'fasta':phy.seqio.write.to_fasta,
+                   'phylip':phy.seqio.write.to_phylip,
+                   'clustal':phy.seqio.write.to_clustal,
+                   'embl':phy.seqio.write.to_embl,
+                   'nexus_seq':phy.seqio.write.to_nexus_seq,
+                   'swiss':phy.seqio.write.to_swiss,
+                   'fastq':phy.seqio.write.to_fastq,
+                   'newick':phy.treeio.write.to_newick,
+                   'nexml':phy.treeio.write.to_nexml,
+                   'nexus_tree':phy.treeio.write.to_nexus_tree}
+
+    file_extension_synonym = {"fa":"fasta",
+                              "fna":"fasta",
+                              "ffn":"fasta",
+                              "faa":"fasta",
+                              "fa":"fasta",
+                              "frn":"fasta",
+                              "aln":"clustal",
+                              "phy":"phylip"}
+
+    # See if the output file exists
+    if os.path.isfile(out_file) and not overwrite:
+        err = "out_file '{}' already exists.\n".format(out_file)
+        raise FileExistsError(err)
+
+    if out_type is None:
+
+        out_type = out_file.split(".")[-1].lower()
+
+        try:
+            out_type = file_extension_synonym[out_type]
+        except KeyError:
+            pass
+
+        try:
+            out_method = out_methods[out_type]
+        except KeyError:
+            err = "\nCould not recognize output type from file extension.\n"
+            err += "Specify type in out_type.\n"
+            raise ValueError(err)
+
+    try:
+        out_method = out_methods[out_type]
+    except KeyError:
+        err = "\n\nout_type '{}' not recognized. Must be one of:\n"
+
+        to_show = out_methods.keys()
+        to_show.sort()
+        for m in to_show:
+            err += "    {}\n".format(m)
+
+        raise ValueError(err)
+
+    if id_col is None:
+        id_col = "uid"
+
+    df_to_write = df.copy()
+    to_write_col = df[id_col]
+    if id_col != "uid":
+        to_write_col = ["{}_{}".format(df_to_write.uid[i],to_write_col[i])
+                        for i in range(len(to_write_col))]
+    df_to_write["to_write_col"] = to_write_col
+
+    out_method(df_to_write,id_col="to_write_col",filename=out_file)
+
+
+def from_external(input_file,df,input_type=None):
+    """
+    Read a phylopandas data frame out to an external file.  If the user wrote
+    the external file out using to_external and did not edit the sequence ids
+    in the extenral file, whatever edits the user made to the alignment or tree
+    can be read back into the source data frame using this function.
+
+    input_file: input file.
+    df: phylopandas dataframe (or copy) used to generate the external file
+    input_type: what type of file to read.  if None, try to figure out from the
+                file extension of the input file
+    """
+    in_methods_seq = {'fasta':phy.seqio.read.read_fasta,
+                      'phylip':phy.seqio.read.read_phylip,
+                      'clustal':phy.seqio.read.read_clustal,
+                      'embl':phy.seqio.read.read_embl,
+                      'nexus_seq':phy.seqio.read.read_nexus_seq,
+                      'swiss':phy.seqio.read.read_swiss,
+                      'fastq':phy.seqio.read.read_fastq}
+
+    in_methods_tree = {'newick':phy.treeio.read.read_newick,
+                       'nexml':phy.treeio.read.read_nexml,
+                       'nexus_tree':phy.treeio.read.read_nexus_tree}
+
+    file_extension_synonym = {"fa":"fasta",
+                              "fna":"fasta",
+                              "ffn":"fasta",
+                              "faa":"fasta",
+                              "fa":"fasta",
+                              "frn":"fasta",
+                              "aln":"clustal",
+                              "phy":"phylip"}
+
+    if input_type is None:
+
+        input_type = input_file.split(".")[-1].lower()
+
+        try:
+            input_type = file_extension_synonym[input_type]
+        except KeyError:
+            pass
+
+        try:
+            in_method = in_methods_seq[input_type]
+        except KeyError:
+
+            try:
+                in_method = in_methods_tree[input_type]
+            except KeyError:
+                err = "\nCould not recognize output type from file extension.\n"
+                err += "Specify type in out_type.\n"
+                raise ValueError(err)
+
+    try:
+        in_method = in_methods_seq[input_type]
+        input_class = "sequence"
+    except KeyError:
+        try:
+            in_method = in_methods_tree[input_type]
+            input_class = "parent"
+        except KeyError:
+
+            err = "\n\ninput_type '{}' not recognized. Must be one of:\n"
+
+            to_show = in_methods.keys()
+            to_show.sort()
+            for m in to_show:
+                err += "    {}\n".format(m)
+
+            raise ValueError(err)
+
+
+
+    new_data = in_method(input_file)
+    uid = [entry.split("_")[0] for entry in new_data.id]
+
+    new_data = pd.DataFrame({"uid":uid,
+                             input_class:new_data[input_class]})
+
+    # Drop the old information from the original frame
+    to_merge = df.copy()
+    to_merge = to_merge.drop(labels=[input_class],axis=1)
+
+    # Merge sequence information back in, this time aligned.
+    output = pd.merge(to_merge, new_data, on=['uid'], how='left')
+
+    return output
